307047308d82b22e353bb8bbc7513744c9d4c429
JagTheFriend <jagthefriend12@gmail.com>
2021-04-09 16:43:44 +0200

    Made the buy command

--- API/Business_Transactions.py
+++ API/Business_Transactions.py
@@ -1,5 +1,29 @@
 from API import utils, save_data
+import json
 
 
 class Business:
-    pass
+    def __init__(self):
+        """
+        Loads the database
+        """
+        with open("API/user_db.json", "r") as file:
+            self.data = json.load(file)
+
+        with open("API/shop_db.json", "r") as file:
+            self.shop = json.load(file)
+    
+    @utils.check()
+    def buy(self, user_id: str, item: str, amount: int) -> bool:
+        if item in self.shop.keys():
+            cost = amount*self.shop[item]["cost"]
+            # check whether the person has enough funds
+            if self.data[user_id]["wallet"] >= cost:
+                self.data[user_id]["wallet"] -= cost
+                # store that in the `inventory`
+                if self.data[user_id]["inventory"][item].get("amount", 0):
+                    self.data[user_id]["inventory"][item]["amount"] += amount
+                else:
+                    self.data[user_id]["inventory"][item]["amount"] = amount
+                return True
+        return False
