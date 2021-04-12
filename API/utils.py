ce4c3db5d0e486d004ef5247ef7caf3b96a99ddc
JagTheFriend <jagthefriend12@gmail.com>
2021-04-12 14:09:43 +0200

    Updated file

--- API/utils.py
+++ API/utils.py
@@ -20,15 +20,15 @@ def to_int(*args) -> list:
 def check(function):
     """Checks whether the account of the user already exists or not"""
 
-    def check_account_exists(self, user_id: str, *args):
+    def check_account_exists(self, *, user_id: str, **args):
         if not user_id in self.data:
             self.data[user_id] = {
                 "wallet": 100,
                 "bank": 500,
                 "inventory": {}
             }
-        args = to_int(*args)
-        return function(self, user_id, *args)
+        args = to_int(**args)
+        return function(self, user_id, **args)
     return check_account_exists
 
 
