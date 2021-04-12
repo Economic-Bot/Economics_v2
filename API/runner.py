b9b151e166b6d1219ca033742922c4ef05d6fe96
JagTheFriend <jagthefriend12@gmail.com>
2021-04-12 14:02:48 +0200

    Using `typing`

--- API/runner.py
+++ API/runner.py
@@ -1,6 +1,6 @@
-from threading import Thread
 from flask import Flask
 from API import Client_Transactions
+import typing as t
 
 app = Flask(__name__)
 
@@ -15,29 +15,29 @@ def main():
 
 @app.route("/with=<user_id>+<amount>")
 @app.route("/withdraw=<user_id>+<amount>")
-def withdraw(user_id: str, amount: int) -> {str: bool, str: float}:
-    return {str: user_transactions.withdraw(user_id, amount)[0]}
+def withdraw(user_id: str, amount: int) -> t.Dict[t.str, bool, str, float, str, None]:
+    ...
 
 
 @app.route("/dep=<user_id>+<amount>")
-def deposit(user_id: str, amount: int) -> {str: bool, str: float}:
-    return {str: user_transactions.dep(user_id, amount)[0]}
+def deposit(user_id: str, amount: int) -> t.Dict[str, bool, str, float, str, None]:
+    ...
 
 
 @app.route("/balance=<user_id>")
 @app.route("/bal=<user_id>")
-def balance(user_id: str) -> {str: bool, str: float}:
-    return {str: user_transactions.balance(user_id)[0]}
+def balance(user_id: str) -> t.Dict[str, bool, str, float, str, None]:
+    ...
 
 
 @app.route("/buy=<user_id>+<item>+<amount>")
-def but(user_id: str, item: str, amount: int) -> {str: bool, str: float}:
-    return {str: user_transactions.buy(user_id, item, amount)[0]}
+def buy(user_id: str, item: str, amount: int) -> t.Dict[str, bool, str, float, str, None]:
+    ...
 
 
 @app.route("/sell=<user_id>+<item>+<amount>")
-def sell(user_id: str, item: str, amount: int) -> {str: bool, str: float}:
-    return {str: user_transactions.sell(user_id, item, amount)[0]}
+def sell(user_id: str, item: str, amount: int) -> t.Dict[str, bool, str, float, str, None]:
+    ...
 
 
 def run():
