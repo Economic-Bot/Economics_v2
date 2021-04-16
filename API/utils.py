3159567ea2a3fcf5d5b988b62ec12c0de2478ef1
JagTheFriend <jagthefriend12@gmail.com>
2021-04-16 18:53:06 +0200

    Fixing a bug, so the program automatically uses the updated database

--- API/utils.py
+++ API/utils.py
@@ -7,7 +7,6 @@ CURRENCY = "⏣"
 
 def to_int(**args) -> dict:
     """Converts the args into a float"""
-    print(args)
     new_dict = {}
     for key, i in zip(args.keys(), args.values()):
         if i.isdigit():
@@ -43,3 +42,18 @@ def save_data(data: dict):
     """Stores the data in the file"""
     with open("API/user_db.json", "w") as file:
         json.dump(data, file, indent=4)
+
+
+def load_data(which_data: str) -> dict:
+    """
+    Loads the database
+    :return: Dictionary
+    """
+    if which_data == "user_data":
+        with open("API/user_db.json", "r") as file:
+            return json.load(file)
+    else:
+        with open("API/shop_db.json", "r") as file:
+            return json.load(file)
+
+    raise FileNotFoundError("Please enter the name of a valid Database ")
