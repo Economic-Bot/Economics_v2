62f616e80834057ae22db12795e7bf8edebae082
JagTheFriend <jagthefriend12@gmail.com>
2021-04-18 09:23:00 +0200

    Fixing bugs

--- API/runner.py
+++ API/runner.py
@@ -21,7 +21,7 @@ def main():
 
 @app.route("/with=<user_id>+<amount>")
 @app.route("/withdraw=<user_id>+<amount>")
-def withdraw(user_id: str, amount: int) ->{str: bool, str: str, str: float, str: str, str: None}:
+def withdraw(user_id: str, amount: int) -> {str: bool, str: str, str: float, str: str, str: None}:
     log.info(f"Received ID: {user_id}, Amount: {amount} in widthdraw")
     return user_transactions.withdraw(user_id=user_id, amount=amount)
 
@@ -41,19 +41,21 @@ def balance(user_id: str) -> {str: bool, str: float, str: None}:
 
 @app.route("/buy=<user_id>+<item>+<amount>")
 def buy(user_id: str, item: str, amount: int) -> {str: bool, str: float, str: None}:
-    log.info(f"Received ID: {user_id}, Amount: {amount}, Item: {item} in buy")
+    log.info(
+        f"Received ID: {user_id}, Amount: {amount}, Item: {item} in buy")
     return user_transactions.buy(user_id=user_id, item=item, amount=amount)
 
 
 @app.route("/sell=<user_id>+<item>+<amount>")
 def sell(user_id: str, item: str, amount: int) -> {str: bool, str: float, str: None}:
-    log.info(f"Received ID: {user_id}, Amount: {amount}, Item: {item} in sell")
+    log.info(
+        f"Received ID: {user_id}, Amount: {amount}, Item: {item} in sell")
     return user_transactions.sell(user_id=user_id, item=item, amount=amount)
 
 
 def run():
     log.critical("Started the host")
-    app.run(port=6969)#host="0.0.0.0", port=8080)
+    app.run(port=6969)  # host="0.0.0.0", port=8080)
 
 
 def start():
