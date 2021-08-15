c58eea88c65eae66ca4cb1866b7fcdd9bf6268fc
JagTheFriend <jagthefriend12@gmail.com>
2021-08-15 19:55:01 +0200

    Added a way to allow a user to  deposit and withdraw coins

--- Commands/UserCommands.py
+++ Commands/UserCommands.py
@@ -1,4 +1,4 @@
-from . import check_user_exists
+from . import USER_DATABASE, check_user_exists, update_database
 from discord.ext.commands import Cog, Context, command
 
 
@@ -7,14 +7,38 @@ class UserCommands(Cog):
         self.bot = bot
 
     @command(alias=["deposit"])
-    def dep(self, ctx: Context, amount: int):
+    async def dep(self, ctx: Context, amount: int):
         """Allows the user to transfer coins from their wallet to the bank"""
         check_user_exists(user_id=ctx.author.id)
+        query = USER_DATABASE.query({"_id": "11"})
+
+        for i in query:
+            bank = i["bank"]
+            wallet = i["wallet"]
+        if amount > wallet:
+            return await ctx.reply("You don't have enough money in your wallet!")
+
+        wallet -= amount
+        bank += amount
+        update_database(user_id=ctx.author.id, new_data={"wallet": wallet, "bank": bank})
+        await ctx.reply(f"You deposited {amount}")
 
     @command(alias=["with"])
-    def withdraw(self, ctx: Context, amount: int):
+    async def withdraw(self, ctx: Context, amount: int):
         """Allows the user to transfer coins from their bank to the wallet"""
         check_user_exists(user_id=ctx.author.id)
+        query = USER_DATABASE.query({"_id": "11"})
+
+        for i in query:
+            bank = i["bank"]
+            wallet = i["wallet"]
+        if amount > bank:
+            return await ctx.reply("You don't have enough money in your wallet!")
+
+        wallet += amount
+        bank -= amount
+        update_database(user_id=ctx.author.id, new_data={"wallet": wallet, "bank": bank})
+        await ctx.reply(f"You deposited {amount}")
 
 
 def setup(bot):
