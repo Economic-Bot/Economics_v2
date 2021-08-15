27976732d90e6b9deadded3a18fcba1f6bc76c95
JagTheFriend <jagthefriend12@gmail.com>
2021-08-15 18:36:19 +0200

    Updated `__init__.py`

--- Commands/__init__.py
+++ Commands/__init__.py
@@ -1,5 +1,27 @@
-def check_user_exists(*, user_id):
-    """Checks whether the user exists in the database or not
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.errors import DuplicateKeyError
+
+
+USERNAME = "bot"
+with open(".env") as file:
+    PASSWORD = file.readlines()[1].split("=")[1]
+
+client = MongoClient(
+    f"mongodb+srv://{USERNAME}:{PASSWORD}"
+    "@cluster0.uniku.mongodb.net/"
+    "myFirstDatabase?retryWrites=true&w=majority"
+)
+db = client.get_database("economicbot")
+USER_DATABASE: Collection = db.get("user_balance")
+
+
+def check_user_exists(*, user_id: int):
+    """Checks whether the user exists in the database or not.
     If it doesn't exists, then it is added to the database
     """
-    ...
+    try:
+        data = {"_id": user_id, "coins": 100, "bank": 150}
+        USER_DATABASE.insert_one(data)
+    except DuplicateKeyError:  # the `user_id` already exists
+        pass
