22498d12dffc88b38c3b9d9e21b03daf7526a1b6
JagTheFriend <jagthefriend12@gmail.com>
2021-08-16 19:27:21 +0200

    Created `%bal` command

--- Commands/UserCommands.py
+++ Commands/UserCommands.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from . import USER_DATABASE, check_user_exists, update_database
+from discord import Member, Embed, Color
 from discord.ext.commands import Cog, Context, command
 
 
@@ -10,11 +12,9 @@ class UserCommands(Cog):
     async def dep(self, ctx: Context, amount: int):
         """Allows the user to transfer coins from their wallet to the bank"""
         check_user_exists(user_id=ctx.author.id)
-        query = USER_DATABASE.query({"_id": "11"})
-
-        for i in query:
-            bank = i["bank"]
-            wallet = i["wallet"]
+        query = USER_DATABASE.find_one({"_id": "11"})
+        bank = query["bank"]
+        wallet = query["wallet"]
         if amount > wallet:
             return await ctx.reply("You don't have enough money in your wallet!")
 
@@ -27,11 +27,9 @@ class UserCommands(Cog):
     async def withdraw(self, ctx: Context, amount: int):
         """Allows the user to transfer coins from their bank to the wallet"""
         check_user_exists(user_id=ctx.author.id)
-        query = USER_DATABASE.query({"_id": "11"})
-
-        for i in query:
-            bank = i["bank"]
-            wallet = i["wallet"]
+        query = USER_DATABASE.find_one({"_id": ctx.author.id})
+        bank = query["bank"]
+        wallet = query["wallet"]
         if amount > bank:
             return await ctx.reply("You don't have enough money in your wallet!")
 
@@ -39,6 +37,29 @@ class UserCommands(Cog):
         bank -= amount
         update_database(user_id=ctx.author.id, new_data={"wallet": wallet, "bank": bank})
         await ctx.reply(f"You deposited {amount}")
+
+    @command(alias=["balance"])
+    async def bal(self, ctx: Context, another_user: Optional[Member] = None):
+        """Allows a user to check their balance"""
+        if another_user:
+            user_id = another_user.id
+            user = another_user
+        else:
+            user_id = ctx.author.id
+            user = ctx.author
+        check_user_exists(user_id=user_id)
+
+        query = USER_DATABASE.find_one({"_id": user_id})
+        bank = query["bank"]
+        wallet = query["wallet"]
+
+        title = (
+            f"Balance of: {user}"
+            f"\nBank: {bank}"
+            f"\nWallet: {wallet}"
+        )
+        embed = Embed(title=title, color=Color.random())
+        await ctx.reply(embed=embed)
 
 
 def setup(bot):
